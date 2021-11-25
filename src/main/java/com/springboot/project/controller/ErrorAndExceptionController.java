7c82d912aad039fd37295d5e5ffcab85beb93b0d
yashwanth1952 <yashwanth.kamana@zemosolabs.com>
2021-11-25 11:24:09 +0530

    after changes v7

--- src/main/java/com/springboot/project/controller/ErrorAndExceptionController.java
+++ src/main/java/com/springboot/project/controller/ErrorAndExceptionController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
